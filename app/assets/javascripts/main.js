019f1f79b2125feb73bc7b5849c5086f477095fd
Onder <onder77@gmail.com>
2017-08-19 02:00:24 +0300

    contents added to restaurant list

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,6 +1,17 @@
 $(function() {
-   $(".search-btn").click(function(){
-     location.href = $(this).attr('href') + $(this).parent().find('input[type=text]').val().replace(/\s/g, '');
+  $(".search-btn").click(function() {
+    location.href = $(this).attr('href') + $(this).parent().find('input[type=text]').val().replace(/\s/g, '');
 
-   });
+  });
+
+  $('.restaurant-list-item').hover(function() {
+    $(this).find('img').hide();
+    $(this).find('.content').show();
+  }, function() {
+    $(this).find('img').show();
+    $(this).find('.content').hide();
+
+  });
+
+
 });
